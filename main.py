cbb8e7bd47d29285fc142191a42a57ed930cf7dc
Wim <poignon.wim@gmx.de>
2021-03-21 23:25:35 +0100

    Genetix Algorithm v3

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar 10 11:26:45 2021
+Created on Wed Mar 10 2021
 
-@author: wimpo
+@author: Wim Poignon TD G
 """
 import random as rd
 from math import *
 from operator import itemgetter
+import time
 # %% Generate the population
 def generate_individual():
-    a=0
-    while a==0: #0 excluded
-        a,b,c=rd.random(),rd.randint(1,20),rd.randint(1,20)
+    a,b,c=rd.randint(1,99)/100,rd.randint(1,20),rd.randint(1,20)
     return [a,b,c]
 
 def generate_population(n):
@@ -19,80 +18,60 @@ def generate_population(n):
     for i in range(n):
         pop.append(generate_individual())
     return pop
-# %%
+# %% Operations
+def mutation(individual):
+    individualNew=individual.copy()
+    proba=rd.random()
+    if(proba<2/3):
+        individualNew[0]=rd.randint(1,99)/100
+    else:
+        index=1 if(rd.randint(0,1)) else 2
+        individualNew[index]=rd.randint(1, 20)
+    return individualNew
+def crossover(indi1,indi2):
+    a1,b1,c1=indi1[0],indi1[1],indi1[2]
+    a2,b2,c2=indi2[0],indi2[1],indi2[2]
+    indi1New=[a2,b1,c2]
+    indi2New=[a1,b2,c1]
+    return indi1New,indi2New
+def select(population,percentBest,percentWorst):
+    tabSelect=[]
+    length=len(population)
+    indexBest=int(length*percentBest/100)
+    indexWorst=int(length*percentWorst/100)
+    for i in range(0,indexBest):
+        tabSelect.append(population[i])
+    for i in range(0, indexWorst):
+        tabSelect.append(population[length-1-i])
+    return tabSelect
+def evaluate(population):
+    tabSorted=sorted(population, key=lambda indi:fitness(indi))
+    return tabSorted
 
-def euclidean_distance(individual):
-    resultW=resultWeierstrass(individual)
-    score=0
-    for element in resultW:
-        score+=pow(element[1]-element[2],2)
-    return sqrt(score)
+def fillpop(population):
+    percentBest,percentWorst,percentMutation=30,10,30
+    tabSelect=select(population,percentBest,percentWorst)
+    cross=[]
+    for i in range(0,len(tabSelect),2):
+        if (i+1<len(population)):
+            cross.append(crossover(population[i],population[i+1])[0])
+            cross.append(crossover(population[i],population[i+1])[1])
+    lengthRest=len(population)-len(tabSelect)-len(cross)
+    newalea=generate_population(lengthRest)
+    newPop=tabSelect[:]+cross[:]+newalea[:]
+    times=int(len(newPop)*percentMutation/100)
+    for i in range(0,times):
+        indexRand=rd.randint(0,len(newPop)-1)
+        newPop[indexRand]=mutation(newPop[indexRand])
+    return newPop
 
-def resultWeierstrass(individual):
-    tab=[]
-    with open("temperature_sample.csv",'r') as file:
-        tabLines=file.read().splitlines()
-        tabLines.pop(0) #delete first line with i and t
-        for element in tabLines:
-            i=float(element.split(";")[0])
-            t=float(element.split(";")[1])
-            tgenerate=weierstrassFunct(individual,i)
-            IRG=[i,t,tgenerate]
-            tab.append(IRG)
-    return tab
-
-def weierstrassFunct(individual,i):
-    result=0
-    for k in range(int(individual[2])):
-        result+=pow(individual[0],k)*cos(individual[1]*pi*i)
-    return result
-
+# %% Functions
 def fitness(individual):
-    return euclidean_distance(individual)
-
-def select(population):
-    indi=population[0]
-    fitness_score=fitness(population[0])
-    for individual in population:
-        if fitness_score>fitness(individual):
-            fitness_score=fitness(individual)
-            indi=individual
-    return indi
-
-def crossover(population):
-    popuCrossover=[]
-    gene=select(population)
-    for i in range(1,len(population)):
-        #newIndi=gene if rd.choice([True,False]) else population[i]
-        crossoverA=gene[0] if rd.choice([True,False]) else population[i][0]
-        crossoverB=gene[1] if rd.choice([True,False]) else population[i][1]
-        crossoverC=gene[2] if rd.choice([True,False]) else population[i][2]
-        newIndi=[crossoverA,crossoverB,crossoverC]
-        popuCrossover.append(newIndi)
-    return popuCrossover
-
-def mutation(population):
-    for individual in population:
-        index=rd.randint(0,2)
-        if(index==0):
-            individual[0]=rd.random()
-        elif(index==1):
-            individual[1]=rd.randint(1,20)
-        else:
-            individual[2]=rd.randint(1,20)
-
-def fillpop(population):
-    populationNew=[]
-    populationNew.append(select(population))
-    populationNew.extend(crossover(population))
-    mutation(populationNew)
-    return populationNew
-"""
-def fitness_score(population):
-    for individual in population:
-        score=euclidean_distance(individual)
-        individual.append(score)
-        #print(score)
+    resultW=resultWeierstrass(individual)
+    score=0
+    for element in resultW:
+        score+=abs(element[1]-element[2])
+    return score/len(resultW)
 
 def resultWeierstrass(individual):
     tab=[]
@@ -107,87 +86,38 @@ def resultWeierstrass(individual):
             tab.append(IRG)
     return tab
 
-def euclidean_distance(individual):
-    resultW=resultWeierstrass(individual)
-    score=0
-    for element in resultW:
-        score+=pow(element[1]-element[2],2)
-    return sqrt(score)
 def weierstrassFunct(individual,i):
     result=0
-    for k in range(int(individual[2])):
-        result+=pow(individual[0],k)*cos(individual[1]*pi*i)
+    for k in range(individual[2]):
+        result+=individual[0]**k*cos(pi*i*individual[1]**k)
     return result
 
-def sort(population):
-    population.sort(key = lambda x: x[3])
-
-def select(population):
-    popu=population.copy()
-    sort(popu)
-    mid=len(popu)//2
-    if len(popu)%2==1:
-        mid+=1
-    # for element in population:
-    #     element.pop(3)
-    return popu[0:mid]
-
-def crossover(population):
-    pop=[]
-    for i in range(0,len(population)-1,2):
-        a=(population[i][0]+population[i+1][0])/2
-        b=(population[i][1]+population[i+1][1])//2
-        c=(population[i][2]+population[i+1][2])//2
-        pop.append([a,b,c])
-    return pop
-
-def mutation(population):
-    for individual in population:
-        mutationIndi(individual)
-
-def mutationIndi(individual):
-    if(rd.choice([True, False])):
-        diff=rd.uniform(-0.3, 0.3)
-        if(individual[0]+diff <1 and individual[0]+diff >0):
-            individual[0]+=diff
-    if(rd.choice([True, False])):
-        diff=rd.randint(-2, 2)
-        if(individual[1]+diff <=20 and individual[1]+diff>=1):
-            individual[1]+=diff
-    #if(rd.choice([True, False])):
-    diff=rd.randint(-5, 5)
-    if(individual[2]+diff <=20 and individual[2]+diff>=1):
-        individual[2]+=diff
-      
-def fillpop(population):
-    fitness_score(population)
-    #print(population[0][3])
-    popN=select(population)+crossover(population)
-    mutation(popN)
-    fitness_score(popN)
-    return popN
-
-def showAll(popN):
-    for i in range(len(popN)):
-        show(popN,i)
-
-def show(popN,index):
-    print("a=",popN[index][0],"b=",popN[index][1],"c=",popN[index][2])    
- 
-def sumScore(popN):
-    scoreS=0
-    for i in range(len(popN)):
-        scoreS+=popN[i][3]
-    return scoreS/len(popN)
-"""
+# %% Algo genetic
 def init():
-    population=generate_population(10)
-    i=0
-    while(fitness(population[0])>1):
-        population=fillpop(population)
-        i+=1
-        #print(fitness(population[0]))    
-        print(population[0])
+    start = time.time()
+    population=generate_population(50)
+    nbriteration=0
+    solutionFound=False
+    count=0
+    individualBefore=population[0]
+    while not solutionFound:
+        evaluation=evaluate(population)
+        nbriteration+=1
+        if(evaluation[0]==individualBefore):
+            count+=1
+        else:
+            count=0
+            individualBefore=evaluation[0]        
+        if(fitness(evaluation[0])<0.02 or count>20):
+            solutionFound=True
+        else:
+            population=fillpop(evaluation)
+    end = time.time()
+    temps=end-start
+    print("Combinaison:",population[0])
+    print("Nombre d'itérations:",nbriteration)
+    print("Temps d'exécution:",round(temps,2),"secondes")
+
 # %% main
 if __name__=="__main__" :
     init()
